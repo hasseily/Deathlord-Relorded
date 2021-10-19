7512cf470eaee731da858f84e7790ccd030de2eb
Henri Asseily <henri@asseily.com>
2021-10-19 12:52:57 +0300

    Added new way of extracting tiles from HGR2 memory

--- Deathlord-Companion/TilesetCreator.cpp
+++ Deathlord-Companion/TilesetCreator.cpp
@@ -1,10 +1,11 @@
 #include "pch.h"
 #include "TilesetCreator.h"
 #include "Emulator/Applewin.h"
-#include "Memory.h"
+#include "Emulator/Memory.h"
 #include <fstream>
 #include <filesystem>
 #include "RemoteControl/RemoteControlManager.h"
+#include <map>
 
 
 void TilesetCreator::start()
@@ -212,4 +213,110 @@ bool TilesetCreator::insertTileInTilesetBuffer(UINT32 iTileId, UINT8 iTileX, UIN
 
 	aKnownTiles[iTileId] = 1;
 	return true;
+}
+
+/// <summary>
+/// Goes through all the tiles in HGR2 and parses them, adding them to the PNG patchwork
+/// </summary>
+/// <returns>Number of tiles inserted</returns>
+UINT TilesetCreator::parseTilesInHGR2()
+{
+    /*
+        The plan is to:
+            - pause the emulator
+            - save the current HGR1 memory for restoring it at the end.
+            - take each tile from HGR2 stored linearly and put it inside HGR1 correctly
+            - run the video renderer
+            - extract from the framebuffer the rendered tile
+            - store it correctly in the tilemap
+            - save the tilemap to disk
+            - restore HGR1
+            - unpause the emulator
+    */
+
+	if (isActive == false)
+		return 0;
+
+	UINT memPtrHGR1Base = 0x2000;
+	UINT memPtrHGR2Base = 0x4000;
+    UINT sizeOfHGR = 0x1fff;
+    UINT tilesParsed = 0;
+
+    g_nAppMode = AppMode_e::MODE_PAUSED;
+
+    LPBYTE snapshotHGR1 = new BYTE[sizeOfHGR];
+    memcpy_s(snapshotHGR1, sizeOfHGR, MemGetMainPtr(memPtrHGR1Base), sizeOfHGR);
+    memset(MemGetMainPtr(memPtrHGR1Base), 0, sizeOfHGR);    // blacken the whole HGR1
+
+	UINT8 tileByteSize = 2 * 16;   // 2 bytes in each of 16 lines
+
+    for (UINT32 iT = 0; iT < (tileByteSize * 0x80); iT += tileByteSize)  // Tile by tile, stop at 128 tiles (the rest is garbage)
+    {
+        for (size_t jT = 0; jT < tileByteSize; jT += 2)     // jump line by line, 2 bytes at a time
+        {
+            // Copy the 2 bytes of each line onto HGR1
+            UINT8 lineNumber = jT / 2;
+            memcpy_s(MemGetMainPtr(memPtrHGR1Base + EIGHTLINEADDRESSES[lineNumber / 8] + SINGLELINEOFFSETS[lineNumber % 8]), 2, MemGetMainPtr(memPtrHGR2Base + iT + jT), 2);
+        }
+
+		// Now get AppleWin to do all the video magic and generate the modern framebuffer data
+		VideoRedrawScreen();
+
+		// And extract the tile from the framebuffer into the tileset framebuffer
+		UINT32 iFBOriginByte = (17 * FRAMEBUFFERWIDTHB) + (LEFTMARGIN * sizeof(UINT32));   // The tile is always the first tile on the top left
+		// Calculate the destination 0,0 byte to draw the tile onto
+		UINT8 tileNumber = iT / tileByteSize;
+		UINT8 iPNGRow = tileNumber / 0x10;
+		UINT8 iPNGCol = tileNumber % 0x10;
+		UINT32 iPNGOriginByte = (iPNGRow * PNGTH * PNGBUFFERWIDTHB) + (iPNGCol * PNGTW * sizeof(UINT32));
+
+		// Map every pixel of the source tile onto the destination tile
+		// But only parse every other line, and skip every other pixel
+		UINT8 b0, b1, b2, b3;   // The individual pixel bytes, low to high
+		UINT iFBCurrentByte;
+		UINT iPNGCurrentByte;
+
+		for (UINT32 j = 0; j < FBTH; j++)
+		{
+			//if (j % 2)
+			//	continue;
+			for (UINT32 i = 0; i < FBTW; i++)
+			{
+			//	if (i % 2)
+			//		continue;
+				iFBCurrentByte = iFBOriginByte + (j * FRAMEBUFFERWIDTHB) + (i * sizeof(UINT32));
+				b0 = g_pFramebufferbits[iFBCurrentByte];
+				b1 = g_pFramebufferbits[iFBCurrentByte + 1];
+				b2 = g_pFramebufferbits[iFBCurrentByte + 2];
+				b3 = g_pFramebufferbits[iFBCurrentByte + 3];
+
+				// Swap the RGB bytes, and force alpha to be opaque because when rgb==ffffff (white), a==00.
+				iPNGCurrentByte = iPNGOriginByte + (j * PNGBUFFERWIDTHB) + (i * sizeof(UINT32));
+				pTilesetBuffer[iPNGCurrentByte] = b2;
+				pTilesetBuffer[iPNGCurrentByte + 1] = b1;
+				pTilesetBuffer[iPNGCurrentByte + 2] = b0;
+				pTilesetBuffer[iPNGCurrentByte + 3] = (char)0xFF;
+			}
+		}
+		++tilesParsed;
+    }
+
+    // Save the tile file
+	std::filesystem::path tilesetPath = std::filesystem::current_path();
+	tilesetPath += "\\Maps\\Deathlord Tileset - Auto.data";
+	std::fstream fsFile(tilesetPath, std::ios::out | std::ios::binary);
+	fsFile.write(pTilesetBuffer, PNGBUFFERSIZE);
+	fsFile.close();
+	std::wstring msg(L"Saved tile file\nRGBA file is at: Deathlord Tileset - Auto.data\nNumber of tiles loaded: ");
+	msg.append(std::to_wstring(iInserted));
+	MessageBox(g_hFrameWindow, msg.c_str(), TEXT("Deathlord Tileset"), MB_OK);
+
+
+    // bring everything back the way it was
+    memcpy_s(MemGetMainPtr(memPtrHGR1Base), sizeOfHGR, snapshotHGR1, sizeOfHGR);
+    delete[] snapshotHGR1;
+    snapshotHGR1 = nullptr;
+
+    g_nAppMode = AppMode_e::MODE_RUNNING;
+	return tilesParsed;
 }
--- Deathlord-Companion/TilesetCreator.h
+++ Deathlord-Companion/TilesetCreator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <windows.h>
+#include <array>
 
 constexpr auto PIXELDEPTH	= 4;	// ARGB
 
@@ -22,14 +23,23 @@ constexpr UINT8 LEFTMARGIN = 20;
 constexpr UINT8 TOPMARGIN = 34;
 
 // For the patchwork PNG
-constexpr UINT8 PNGTW = 14;
-constexpr UINT8 PNGTH = 16;
+constexpr UINT8 PNGTW = 28;
+constexpr UINT8 PNGTH = 32;
 constexpr UINT8 PNGTILESPERROW = 16;
 constexpr UINT8 PNGTILESPERCOL	= 16;
 constexpr auto PNGBUFFERWIDTHB = PNGTW * PNGTILESPERROW * sizeof(UINT32);
 constexpr auto PNGBUFFERHEIGHT = PNGTH * PNGTILESPERCOL;
 constexpr auto PNGBUFFERSIZE = PNGBUFFERWIDTHB * PNGBUFFERHEIGHT;
 
+// For parsing the tileset in HGR2
+constexpr std::array<const uint16_t, 192 / 8> EIGHTLINEADDRESSES = {
+	0x0000, 0x0080, 0x0100, 0x0180, 0x0200, 0x0280, 0x0300, 0x0380,
+	0x0028, 0x00a8, 0x0128, 0x01a8, 0x0228, 0x02a8, 0x0328, 0x03a8,
+	0x0050, 0x00d0, 0x0150, 0x01d0, 0x0250, 0x02d0, 0x0350, 0x03d0
+};
+constexpr std::array<const uint16_t, 8> SINGLELINEOFFSETS = {
+	0x0, 0x400, 0x800, 0xc00, 0x1000, 0x1400, 0x1800, 0x1c00
+};
 
 class TilesetCreator
 {
@@ -39,6 +49,7 @@ public:
 	TilesetCreator()
 	{
 		pTilesetBuffer = new char[PNGBUFFERSIZE];
+		memset(pTilesetBuffer, 0, PNGBUFFERSIZE);
 	}
 	~TilesetCreator()
 	{
@@ -49,6 +60,7 @@ public:
 	void reset();
 	UINT parseTilesInFrameBuffer();
 	bool insertTileInTilesetBuffer(UINT32 iTileId, UINT8 iTileX, UINT8 iTileY);
+	UINT parseTilesInHGR2();
 	void readTileFile();
 	void saveTileFile();
 private:
