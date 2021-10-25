0d6663ccfba779a07b66522b7d0778bdb604cb3e
Henri Asseily <henri@asseily.com>
2021-10-25 21:45:37 +0300

    WinPix support

--- Deathlord-Companion/Main.cpp
+++ Deathlord-Companion/Main.cpp
@@ -19,6 +19,9 @@
 #include "Emulator/RGBMonitor.h"
 #include "DeathlordHacks.h"
 #include "TilesetCreator.h"
+// For WinPixGpuCapture
+#include <filesystem>
+#include <shlobj.h>
 
 using namespace DirectX;
 
@@ -65,6 +68,35 @@ static void ExceptionHandler(LPCSTR pError)
 		wc,
 		TEXT("Runtime Exception"),
 		MB_ICONEXCLAMATION | MB_SETFOREGROUND);
+}
+
+static std::wstring GetLatestWinPixGpuCapturerPath()
+{
+	LPWSTR programFilesPath = nullptr;
+	SHGetKnownFolderPath(FOLDERID_ProgramFiles, KF_FLAG_DEFAULT, NULL, &programFilesPath);
+
+	std::filesystem::path pixInstallationPath = programFilesPath;
+	pixInstallationPath /= "Microsoft PIX";
+
+	std::wstring newestVersionFound;
+
+	for (auto const& directory_entry : std::filesystem::directory_iterator(pixInstallationPath))
+	{
+		if (directory_entry.is_directory())
+		{
+			if (newestVersionFound.empty() || newestVersionFound < directory_entry.path().filename().c_str())
+			{
+				newestVersionFound = directory_entry.path().filename().c_str();
+			}
+		}
+	}
+
+	if (newestVersionFound.empty())
+	{
+		// TODO: Error, no PIX installation found
+	}
+
+	return pixInstallationPath / newestVersionFound / L"WinPixGpuCapturer.dll";
 }
 
 std::unique_ptr<Game>* GetGamePtr()
@@ -144,6 +176,15 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	HACCEL haccel;      // handle to accelerator table 
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	/*
+	// Check to see if a copy of WinPixGpuCapturer.dll has already been injected into the application.
+	// This may happen if the application is launched through the PIX UI. 
+	if (GetModuleHandle(L"WinPixGpuCapturer.dll") == 0)
+	{
+		LoadLibrary(GetLatestWinPixGpuCapturerPath().c_str());
+	}
+	*/
 
 	try {
 		// Initialize global strings
