88d99fe3c83062536e74b23957882e45d545acd0
Henri Asseily <henri@asseily.com>
2021-11-11 07:23:29 +0200

    Comments

--- Deathlord-Companion/Emulator/CPU.h
+++ Deathlord-Companion/Emulator/CPU.h
@@ -3,7 +3,7 @@
 #include "Common.h"
 
 
-// Memory locations to hijack when processing the game
+// Program Counter locations to hijack when processing the game
 // to change the behavior on the fly without patching the original code
 #define PC_DECREMENT_TIMER		0x621F		// routine to decrement a timer before it makes the player "wait" and pass a turn
 #define PC_TRANSIT_OUT_OVERLAND	0xE8F4		// routine that gets the player into a dungeon or town
@@ -11,6 +11,8 @@
 #define PC_BEGIN_DRAWING_TILES	0x5931		// Beginning of the tiles drawing routine on the main screen. $300-350 contains the "current" map, $351-3A0 contains the "new" map
 #define PC_END_DRAWING_TILES	0x586A		// After all the drawing is done and the LOS is updated. $300-350 contains the map
 #define PC_CHECK_REAR_ATTACK	0xA7EF		// Check for the rear rank not allowed to attack
+
+// Memory Locations used for hijacking
 #define MEM_CURRENT_CHAR_POS	0xFC21		// Current active character position (1-6)
 #define MEM_CHAR_1_WEAP_READY	0xFDEA		// Character 1 weapon ready status: ff = fists, 00 = melee, 01 = ranged
 
