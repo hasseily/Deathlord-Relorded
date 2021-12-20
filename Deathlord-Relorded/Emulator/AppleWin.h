82f6cab4892efd8088d57e98f2600d96bdb4fa3d
Henri Asseily <henri@asseily.com>
2021-12-20 18:50:08 +0200

    Fork of Deathlord Companion. Initial commit.

---
+++ Deathlord-Relorded/Emulator/AppleWin.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "Card.h"
+#include "Common.h"
+#include "Memory.h"
+#include "Mockingboard.h"
+#include "SoundCore.h"
+#include "Speaker.h"
+#include "Video.h"
+#include <string>
+#include "RemoteControl/RemoteControlManager.h"
+
+static std::wstring g_pAppTitle = L"Deathlord Relorded";
+
+extern RemoteControlManager g_RemoteControlMgr;			// RIK -- Necessary to have a single instance only
+
+extern eApple2Type g_Apple2Type;
+eApple2Type GetApple2Type(void);
+void SetApple2Type(eApple2Type type);
+
+double Get6502BaseClock(void);
+void SetCurrentCLK6502(void);
+
+void SingleStep(bool bReinit);
+
+UINT GetFrameBufferWidth();
+UINT GetFrameBufferHeight();
+UINT GetFrameBufferBorderWidth();
+UINT GetFrameBufferBorderHeight();
+
+void ApplyNonVolatileConfig();
+void EmulatorOneTimeInitialization(HWND window);
+void EmulatorRepeatInitialization();
+void EmulatorReboot();
+void EmulatorMessageLoopProcessing();
+UINT8 EmulatorGetSpeed();
+void EmulatorSetSpeed(UINT8 speed);
+
+void UseClockMultiplier(double clockMultiplier);
+
+extern bool       g_bFullSpeed;
+extern UINT64	  g_debug_video_field;	// defined in Game.cpp // shows debug video info on screen
+extern UINT64	  g_debug_video_data;	// defined in Game.cpp // shows debug video info on screen
+
+
+//===========================================
+
+// Win32
+extern HINSTANCE  g_hInstance;
+extern HWND g_hFrameWindow;
+
+extern AppMode_e g_nAppMode;
+
+extern std::string g_sProgramDir;
+extern std::string g_sCurrentDir;
+
+extern bool       g_bRestart;
+
+extern DWORD      g_dwSpeed;
+extern double     g_fCurrentCLK6502;
+
+extern int        g_nCpuCyclesFeedback;
+extern DWORD      g_dwCyclesThisFrame;
+
+extern bool       g_bDisableDirectSound;				// Cmd line switch: don't init DS (so no MB/Speaker support)
+extern bool       g_bDisableDirectSoundMockingboard;	// Cmd line switch: don't init MB support
+extern int        g_nMemoryClearType;					// Cmd line switch: use specific MIP (Memory Initialization Pattern)
+
+extern class CardManager& GetCardMgr(void);
+extern class SynchronousEventManager g_SynchronousEventMgr;
---
+++ Deathlord-Relorded/NonVolatile.cpp
@@ -0,0 +1,133 @@
+#include "pch.h"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <filesystem>
+#include <shobjidl_core.h> 
+#include "NonVolatile.h"
+#include "nlohmann/json.hpp"
+#include "HAUtils.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+static nlohmann::json nv_json = R"(
+  {
+    "profilePath": "Profiles\\Deathlord_Default.json",
+    "hdvPath":			  "",
+    "diskBootPath":		  "Images\\Deathlord - Disk 1, Side A Boot disk.woz",
+    "diskScenAPath":	  "Images\\Deathlord Scenario A.nib",
+    "diskScenBPath":	  "Images\\Deathlord Scenario B.nib",
+	"speed":			  1,
+	"scanlines":		  false,
+	"showMap":			  true,
+	"showFog":			  true,
+	"showFootsteps":	  true,
+	"showHidden":		  true,
+	"showSpells":		  true,
+	"video":			  0,
+    "volumeSpeaker":	  2,
+	"useGameLink":        true,
+	"logCombat":		  false
+  }
+)"_json;
+
+static nlohmann::json nvmarkers_json = R"(
+	{
+		"fogOfWarMarkers":	  {}
+	}
+)"_json;
+
+static std::string configfilename = "DeathlordRelorded.conf";
+static std::string markersfilename = "dcmarkers.data";	// contains markers data (8 bits per tile) for all maps
+
+int NonVolatile::SaveToDisk()
+{
+	std::string sprofPath;
+	std::string sDiskBootPath;
+	std::string sDiskScenAPath;
+	std::string sDiskScenBPath;
+	HA::ConvertWStrToStr(&profilePath, &sprofPath);
+	HA::ConvertWStrToStr(&diskBootPath, &sDiskBootPath);
+	HA::ConvertWStrToStr(&diskScenAPath, &sDiskScenAPath);
+	HA::ConvertWStrToStr(&diskScenBPath, &sDiskScenBPath);
+
+	nv_json["profilePath"]			= sprofPath;
+	nv_json["diskBootPath"]			= sDiskBootPath;
+	nv_json["diskScenAPath"]		= sDiskScenAPath;
+	nv_json["diskScenBPath"]		= sDiskScenBPath;
+	nv_json["speed"]				= speed;
+	nv_json["scanlines"]			= scanlines;
+	nv_json["showMap"]				= showMap;
+	nv_json["showFog"]				= showFog;
+	nv_json["showFootsteps"]		= showFootsteps;
+	nv_json["showHidden"]			= showHidden;
+	nv_json["showSpells"]			= showSpells;
+	nv_json["video"]				= video;
+	nv_json["volumeSpeaker"]		= volumeSpeaker;
+	nv_json["useGameLink"]			= useGameLink;
+	nv_json["logCombat"]			= logCombat;
+	std::ofstream out(configfilename);
+	out << std::setw(4) << nv_json << std::endl;
+	out.close();
+
+	// save markers independently
+	nvmarkers_json["fogOfWarMarkers"] = fogOfWarMarkers;
+	out.open(markersfilename);
+	out << std::setw(0) << nvmarkers_json << std::endl;
+	out.close();
+
+	return 0;
+}
+
+int NonVolatile::LoadFromDisk()
+{
+	nlohmann::json j;
+	std::ifstream in(configfilename);
+	if (!in.fail())
+	{
+		in >> j;
+		in.close();
+		nv_json.merge_patch(j);
+	}
+	// load markers
+	j.clear();
+	in.open(markersfilename);
+	if (!in.fail())
+	{
+		in >> j;
+		in.close();
+		nvmarkers_json.merge_patch(j);
+	}
+
+	std::string _profilePath = nv_json["profilePath"].get<std::string>();
+	if (_profilePath.size() == 0) {
+		fs::path defaultPath = fs::current_path();
+		defaultPath += "\\Profiles\\Deathlord_Default.json";
+		_profilePath.assign(defaultPath.string());
+	}
+	HA::ConvertStrToWStr(&_profilePath, &profilePath);
+	std::string _diskBootPath = nv_json["diskBootPath"].get<std::string>();
+	HA::ConvertStrToWStr(&_diskBootPath, &diskBootPath);
+	std::string _diskScenAPath = nv_json["diskScenAPath"].get<std::string>();
+	HA::ConvertStrToWStr(&_diskScenAPath, &diskScenAPath);
+	std::string _diskScenBPath = nv_json["diskScenBPath"].get<std::string>();
+	HA::ConvertStrToWStr(&_diskScenBPath, &diskScenBPath);
+
+	speed = nv_json["speed"].get<int>();
+	scanlines = nv_json["scanlines"].get<bool>();
+	showMap = nv_json["showMap"].get<bool>();
+	showFog = nv_json["showFog"].get<bool>();
+	showFootsteps = nv_json["showFootsteps"].get<bool>();
+	showHidden = nv_json["showHidden"].get<bool>();
+	showSpells = nv_json["showSpells"].get<bool>();
+	video = nv_json["video"].get<int>();
+	volumeSpeaker = nv_json["volumeSpeaker"].get<int>();
+	useGameLink = nv_json["useGameLink"].get<bool>();
+	logCombat = nv_json["logCombat"].get<bool>();
+
+	// markers
+	fogOfWarMarkers = nvmarkers_json["fogOfWarMarkers"].get<std::map<std::string, std::vector<UINT8>>>();
+
+	return 0;
+}
