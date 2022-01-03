395bce56311179df5490ae78171ddee08f07d920
Henri Asseily <henri@asseily.com>
2022-01-03 11:29:28 +0200

    Changed pin angle and shadow

--- Deathlord-Relorded/MiniMap.h
+++ Deathlord-Relorded/MiniMap.h
@@ -9,7 +9,7 @@ using namespace std;
 
 // Where to put the pin on the minimap.
 constexpr UINT16 MINIMAP_ORIGIN_X = 1297;
-constexpr UINT16 MINIMAP_ORIGIN_Y = 44;
+constexpr UINT16 MINIMAP_ORIGIN_Y = 42;
 constexpr float MINIMAP_X_INCREMENT = 17.5f;
 constexpr float MINIMAP_Y_INCREMENT = 20.f;
 
