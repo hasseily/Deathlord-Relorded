a1854cfa11fb4d154d4faa73fc27e96b86cf9957
Henri Asseily <henri@asseily.com>
2022-03-04 11:12:30 +0200

    All modified items (saved games, etc...) are now stored in the "Saved Games" special user directory on windows.

--- Deathlord-Relorded/NonVolatile.cpp
+++ Deathlord-Relorded/NonVolatile.cpp
@@ -7,6 +7,8 @@
 #include "NonVolatile.h"
 #include "nlohmann/json.hpp"
 #include "HAUtils.h"
+#include <shlobj.h>
+#include <objbase.h>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -37,11 +39,49 @@ static nlohmann::json nvmarkers_json = R"(
 	}
 )"_json;
 
-static std::string configfilename = "DeathlordRelorded.conf";
-static std::string markersfilename = "dcmarkers.data";	// contains markers data (8 bits per tile) for all maps
+static std::wstring configfilename = L"DeathlordRelorded.conf";
+static std::wstring markersfilename = L"dcmarkers.data";	// contains markers data (8 bits per tile) for all maps
+
+bool FileExists(LPCTSTR szPath)
+{
+	DWORD dwAttrib = GetFileAttributes(szPath);
+
+	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+		(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
 
 int NonVolatile::SaveToDisk()
 {
+	// Determine saved games folder
+	std::wstring savedGamesPath;
+	PWSTR path = NULL;
+	HRESULT r = SHGetKnownFolderPath(FOLDERID_SavedGames, KF_FLAG_CREATE, NULL, &path);
+	if (path != NULL)
+	{
+		savedGamesPath.assign(path);
+		savedGamesPath.append(L"\\Deathlord Relorded\\");
+		CoTaskMemFree(path);
+
+		auto _dbfn = fs::path(diskBootPath).filename().wstring();
+		if (!FileExists((savedGamesPath + _dbfn).c_str()))
+		{
+			CopyFile(diskBootPath.c_str(), (savedGamesPath + _dbfn).c_str(), true);
+			diskBootPath = savedGamesPath + _dbfn;
+		}
+		auto _safn = fs::path(diskScenAPath).filename().wstring();
+		if (!FileExists((savedGamesPath + _safn).c_str()))
+		{
+			CopyFile(diskScenAPath.c_str(), (savedGamesPath + _safn).c_str(), true);
+			diskScenAPath = savedGamesPath + _safn;
+		}
+		auto _sbfn = fs::path(diskScenBPath).filename().wstring();
+		if (!FileExists((savedGamesPath + _sbfn).c_str()))
+		{
+			CopyFile(diskScenBPath.c_str(), (savedGamesPath + _sbfn).c_str(), true);
+			diskScenBPath = savedGamesPath + _sbfn;
+		}
+	}
+
 	std::string sDiskBootPath;
 	std::string sDiskScenAPath;
 	std::string sDiskScenBPath;
@@ -62,14 +102,14 @@ int NonVolatile::SaveToDisk()
 	nv_json["volumeSpeaker"]		= volumeSpeaker;
 	nv_json["useGameLink"]			= useGameLink;
 	nv_json["logCombat"]			= logCombat;
-	std::ofstream out(configfilename);
+	std::ofstream out(savedGamesPath + configfilename);
 	out << std::setw(4) << nv_json << std::endl;
 	out.close();
 
 	// save markers and other map data independently
 	nvmarkers_json["fogOfWarMarkers"] = fogOfWarMarkers;
 	nvmarkers_json["sectorsSeen"] = sectorsSeen;
-	out.open(markersfilename);
+	out.open(savedGamesPath + markersfilename);
 	out << std::setw(0) << nvmarkers_json << std::endl;
 	out.close();
 
@@ -78,8 +118,28 @@ int NonVolatile::SaveToDisk()
 
 int NonVolatile::LoadFromDisk()
 {
+	// Determine saved games folder
+	std::wstring savedGamesPath;
+	PWSTR path = NULL;
+
+	HRESULT r = SHGetKnownFolderPath(FOLDERID_SavedGames, KF_FLAG_CREATE, NULL, &path);
+	if (path != NULL)
+	{
+		savedGamesPath.assign(path);
+		savedGamesPath.append(L"\\Deathlord Relorded\\");
+		CoTaskMemFree(path);
+
+		// Try to create the save game dir. If it already exists, use it
+		BOOL bRes = CreateDirectory(savedGamesPath.c_str(), NULL);
+		if (bRes != 0)
+		{
+			// The saved games dir didn't exist, load from default game dir
+			savedGamesPath = L"";
+		}
+	}
+
 	nlohmann::json j;
-	std::ifstream in(configfilename);
+	std::ifstream in(savedGamesPath + configfilename);
 	if (!in.fail())
 	{
 		in >> j;
@@ -88,7 +148,7 @@ int NonVolatile::LoadFromDisk()
 	}
 	// load markers
 	j.clear();
-	in.open(markersfilename);
+	in.open(savedGamesPath + markersfilename);
 	if (!in.fail())
 	{
 		in >> j;
